89ea37075210db41379e2b46472629d5ccce99bd
Ankit Gupta <ankitmanojg@gmail.com>
2025-10-22 21:31:05 +0530

    workflows-crud

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,30 +1,9 @@
-import prisma from "@/lib/db";
-import { baseProcedure, createTRPCRouter, premiumProcedure, protectedProcedure } from "../init";
-import { inngest } from "@/inngest/client";
-import { google } from '@ai-sdk/google';
-import { generateText } from 'ai';
-import { TRPCError } from "@trpc/server";
+
+import { createTRPCRouter } from "../init";
+
+import { workflowsRouter } from "@/features/workflows/server/routers";
 
 export const appRouter = createTRPCRouter({
-    testAi: premiumProcedure.mutation(async () => {
-        throw new TRPCError({ code: "BAD_REQUEST", message: "Something went wrong" });
-        await inngest.send({
-            name: "execute/ai",
-        });
-        return { success: true, message: "Job queued" }
-    }),
-    getWorkflows: protectedProcedure.query(({ ctx }) => {
-        return prisma.workflow.findMany();
-    }),
-    createWokflow: protectedProcedure.mutation(async () => {
-        await inngest.send({
-            name: "test/hello.world",
-            data: {
-                email: "ankit@mail.com",
-            },
-        });
-
-        return { success: true, message: "Job queued" }
-    }),
+    workflows: workflowsRouter,
 });
 export type AppRouter = typeof appRouter;
