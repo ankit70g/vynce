e1edb2b20c7753672388a5f63b1b24e1131ba156
Ankit Gupta <ankitmanojg@gmail.com>
2025-10-22 14:24:02 +0530

    payments

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,12 +1,12 @@
 import prisma from "@/lib/db";
-import { baseProcedure, createTRPCRouter, protectedProcedure } from "../init";
+import { baseProcedure, createTRPCRouter, premiumProcedure, protectedProcedure } from "../init";
 import { inngest } from "@/inngest/client";
 import { google } from '@ai-sdk/google';
 import { generateText } from 'ai';
 import { TRPCError } from "@trpc/server";
 
 export const appRouter = createTRPCRouter({
-    testAi: baseProcedure.mutation(async () => {
+    testAi: premiumProcedure.mutation(async () => {
         throw new TRPCError({ code: "BAD_REQUEST", message: "Something went wrong" });
         await inngest.send({
             name: "execute/ai",
