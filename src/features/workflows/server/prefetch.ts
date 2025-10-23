bc957ca36cdbbbf1889460b246e281228144533a
Ankit Gupta <ankitmanojg@gmail.com>
2025-10-23 19:09:07 +0530

    workflow-page

--- src/features/workflows/server/prefetch.ts
+++ src/features/workflows/server/prefetch.ts
@@ -4,7 +4,12 @@ import { inferInput } from "@trpc/tanstack-react-query";
 
 type Input = inferInput<typeof trpc.workflows.getMany>;
 
-
+// Prefetch all workflows
 export const prefetchWorkflows = (params: Input) => {
     return prefetch(trpc.workflows.getMany.queryOptions(params));
+};
+
+// Prefetch a single workflow
+export const prefetchWorkflow = (id: string) => {
+    return prefetch(trpc.workflows.getOne.queryOptions({ id }));
 };
