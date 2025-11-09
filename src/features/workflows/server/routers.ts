f82f5b91916a0af2d116a5a046eb07af834cc510
Ankit Gupta <ankitmanojg@gmail.com>
2025-11-09 12:59:09 +0530

    node-execution

--- src/features/workflows/server/routers.ts
+++ src/features/workflows/server/routers.ts
@@ -1,6 +1,7 @@
 import { Input } from "@/components/ui/input";
 import { PAGINATION } from "@/config/constants";
 import { NodeType } from "@/generated/prisma";
+import { inngest } from "@/inngest/client";
 import prisma from "@/lib/db";
 import { createTRPCRouter, premiumProcedure, protectedProcedure } from "@/trpc/init";
 import type { Node, Edge } from "@xyflow/react";
@@ -9,6 +10,22 @@ import { generateSlug } from "random-word-slugs";
 import z from "zod";
 
 export const workflowsRouter = createTRPCRouter({
+    execute: protectedProcedure
+        .input(z.object({ id: z.string() }))
+        .mutation(async ({ input, ctx }) => {
+            const workflow = await prisma.workflow.findUniqueOrThrow({
+                where: {
+                    id: input.id,
+                    userId: ctx.auth.user.id,
+                },
+            });
+
+            await inngest.send({
+                name: "workflows/execute.workflow",
+                data: { workflowId: input.id },
+            });
+            return workflow;
+        }),
     create: premiumProcedure.mutation(({ ctx }) => {
         return prisma.workflow.create({
             data: {
