ad13e93b33f1f8ae18afb8d3d39f5f5ecce68086
Ankit Gupta <ankitmanojg@gmail.com>
2025-11-16 19:07:42 +0530

    update

--- next.config.ts
+++ next.config.ts
@@ -3,9 +3,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   devIndicators: false,
-  typescript: {
-    ignoreBuildErrors: true,
-  },
   async redirects() {
     return [
       {
