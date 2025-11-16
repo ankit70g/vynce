b6cff6b457e9361be593ae3b7c20aa1828a90d36
Ankit Gupta <ankitmanojg@gmail.com>
2025-11-16 12:59:45 +0530

    discord-slack-node

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -12,6 +12,8 @@ import { stripeTriggerChannel } from "./channels/stripe-trigger";
 import { geminiChannel } from "./channels/gemini";
 import { openAiChannel } from "./channels/openai";
 import { anthropicChannel } from "./channels/anthropic";
+import { discordChannel } from "./channels/discord";
+import { slackChannel } from "./channels/slack";
 
 export const executeWorkflow = inngest.createFunction(
     {
@@ -29,6 +31,8 @@ export const executeWorkflow = inngest.createFunction(
             geminiChannel(),
             openAiChannel(),
             anthropicChannel(),
+            discordChannel(),
+            slackChannel(),
         ],
     },
     async ({ event, step, publish }) => {
@@ -50,6 +54,16 @@ export const executeWorkflow = inngest.createFunction(
             return topologicalSort(workflow.nodes, workflow.connections);
         });
 
+        const userId = await step.run("find-user-id", async () => {
+            const workflow = await prisma.workflow.findUniqueOrThrow({
+                where: { id: workflowId },
+                select: {
+                    userId: true,
+                },
+            });
+            return workflow.userId;
+        });
+
         // Initialize the context with any initial data from the trigger
         let context = event.data.initialData || {};
 
@@ -59,6 +73,7 @@ export const executeWorkflow = inngest.createFunction(
             context = await executor({
                 data: node.data as Record<string, unknown>,
                 nodeId: node.id,
+                userId,
                 context,
                 step,
                 publish,
